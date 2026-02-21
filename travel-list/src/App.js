05e50895e6536d2c4101547d212f09d673655b12
Judexify <olukojuoluwadunsi12@gmail.com>
2026-02-21 17:53:13 +0100

    finished wiith this

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,7 +1,9 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 2, description: "Charger", quantity: 1, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: true },
 ];
 
 export default function App() {
@@ -19,10 +21,43 @@ function Logo() {
   return <h1>🏝️ Far Away 💼</h1>;
 }
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(6);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+    setDescription("");
+    setQuantity(1);
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-    </div>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array(20)
+          .fill()
+          .map((_, index) => index + 1)
+          .map((num) => (
+            <option value={num} key={num}>
+              {num}
+            </option>
+          ))}
+      </select>
+      <input
+        type="text"
+        placeholder="item.."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      ></input>
+      <button>ADD</button>
+    </form>
   );
 }
 function PackingList() {
@@ -30,7 +65,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
@@ -55,3 +90,7 @@ function Stats() {
     </footer>
   );
 }
+// three steps of using state
+// define
+// use the state variable
+// update the state variable
