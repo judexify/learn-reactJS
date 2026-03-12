60c55ff3e57e2673160a84786cf698b85311a620
Judexify <olukojuoluwadunsi12@gmail.com>
2026-03-12 22:04:53 +0100

    refactor: enhance StarRating component with customizable props and loading states

--- reusable-star-compo/src/index.js
+++ reusable-star-compo/src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import StarRating from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <StarRating color="#fcc419" />
   </React.StrictMode>,
 );
